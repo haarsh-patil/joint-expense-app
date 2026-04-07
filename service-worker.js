075813f75bf6a1dd48852d6bdbd58706c12726f5
haarsh-patil <harsh.patil195@gmail.com>
2026-04-07 16:41:23 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,28 @@
 self.addEventListener("install", function(e) {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open("expense-app").then(function(cache) {
+    caches.open("expense-app-v2").then(function(cache) {
       return cache.addAll([
         "./",
         "./index.html",
         "./manifest.json",
-        "./icons/app-icon.svg",
+        "./app-icon.svg",
       ]);
     })
   );
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key !== "expense-app-v2") {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
